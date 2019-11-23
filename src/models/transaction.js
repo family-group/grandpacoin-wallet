a3ee3ed7c46499708bfec6e9f6f7f0759ed16bc1
Ariana Ocando <arianaocando@oxoft.io>
2019-11-23 16:34:42 -0400

    implemented transaction method

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,36 +1,92 @@
 import utils from '../utils/functions';
-import Ajax from '../utils/xhr';
+import Xhr from '../utils/xhr';
+import { sha256 } from '../utils/hashes';
 
 class Transaction {
-    constructor(sender, to, value, fee, data, senderPubKey) {
-        this.from = sender;
-        this.to = to;
+    constructor({ from, to, value, data, senderPubKey, privKey }) {
+        this.from = from.replace('0x', '');
+        this.to = to.replace('0x', '');
         this.value = value;
-        this.fee = fee;
+        this.fee = "10";
         this.dateCreated = new Date().toISOString();
-        this.data = data;
-        this.senderPubKey = senderPubKey;
+        this.data = data ? data.trim() : undefined;
+        this.senderPubKey = senderPubKey.replace('0x', '');
+        this.privKey = privKey.replace('0x', '');
+        this.senderSignature = Transaction.sign(this);
+        this.transactionDataHash = Transaction.transactionHash(this)
     }
 
-    sign(privateKey) {
-        this.senderSignature = utils.signTransaction(this, privateKey);
+    getTransactionData() {
+        const { from, to, value, data, fee, senderPubKey, senderSignature, dateCreated, transactionDataHash } = this;
+        return {
+            from,
+            to,
+            value,
+            fee,
+            dateCreated,
+            data,
+            senderPubKey,
+            transactionDataHash,
+            senderSignature
+        }
+    }
+    static transactionHash(transaction) {
+        const { from, to, value, fee, data, dateCreated, senderPubKey } = transaction;
+        return sha256(JSON.stringify({
+            from,
+            to,
+            value,
+            fee,
+            dateCreated,
+            data: data ? data : undefined,
+            senderPubKey,
+        }))
+    }
+
+    static sign(transaction) {
+        const { from, to, value, fee, data, dateCreated, senderPubKey, privKey } = transaction;
+        return utils.signTransaction({
+            from,
+            to,
+            value,
+            fee,
+            dateCreated,
+            data: data ? data : undefined,
+            senderPubKey
+        }, privKey);
     }
 
     verify() {
         return utils.verifySignature(this.transactionDataHash, this.senderPubKey, this.senderSignature);
     }
+    //0xa6ef9089840a55ae5934b49e681ca6a60a7ebaec
+    //0x607168b61015cfe766a3a6716180f9b60e909f35
+    async send(providerUrl) {
+        const { from, to, value, data, fee, senderPubKey, senderSignature, dateCreated, transactionDataHash } = this;
+        try {
+            const xhr = new Xhr('/transactions/send', {
+                useBaseUrl: providerUrl,
+                body: {
+                    from,
+                    to,
+                    value,
+                    fee,
+                    dateCreated,
+                    data,
+                    senderPubKey,
+                    transactionDataHash,
+                    senderSignature
+                },
+                method: 'POST'
+            });
 
-    send(providerUrl) {
-        const ajax = new Ajax(providerUrl + '/transactions/send', {
-            method: 'post',
-            body: this,
-            headers: {
-                'Content-Type': 'application/json'
+            return {
+                promise: xhr.result(),
+                result: await xhr.result(),
+                abort: xhr.abort
             }
-        });
-        return {
-            promise: ajax.result(),
-            abort: ajax.abort
+        } catch (err) {
+            console.log(err)
         }
     }
 }
