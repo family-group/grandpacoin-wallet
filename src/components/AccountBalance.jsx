d32830159ffaa140be8a7a3e88347d1ebc88e724
Ariana Ocando <arianaocando@oxoft.io>
2019-11-23 17:32:59 -0400

    added open wallet method

--- src/components/AccountBalance.jsx
+++ src/components/AccountBalance.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Loader from './Loader';
 import Layout from './Layout';
 import Button from './Button';
 import Xhr from '../utils/xhr';
-import { isValidUrl, isValidAddress } from '../utils/validator';
-import LogAreaOutput from './LogAreaOutput';
 import TextInput from './TextInput';
+import Wallet from './../models/wallet';
+import LogAreaOutput from './LogAreaOutput';
+import { isValidUrl } from '../utils/validator';
+import { getWalletJSON } from './../utils/functions';
+
 import './css/AccountBalance.css';
 
 const styles = {
@@ -30,47 +34,34 @@ class AccountBalance extends React.Component {
             pendingBalance: '',
         }
         this.state = {
+            address: '',
             balanceStatus: false,
+            disabled: false,
+            active: false,
             ...this.balancesRequest,
             ...this.inputErrors
         };
         this.onChange = this.onChange.bind(this);
-        this.getInputErrors = this.getInputErrors.bind(this);
         this.onClick = this.onClick.bind(this);
+        this.openWallet = this.openWallet.bind(this);
+        this.openWalletWithPassword = this.openWalletWithPassword.bind(this);
     }
-
-    getInputErrors() {
-        if (!isValidAddress(this.address) && !isValidUrl(this.nodeUrl)) {
-            return this.inputErrors = {
-                addressInput: 'Invalid Address! Please try again.',
-                nodeInput: 'Invalid Url! Please try again.'
-            }
-        }
-        if (!isValidAddress(this.address) && isValidUrl(this.nodeUrl)) {
-            return this.inputErrors = {
-                addressInput: 'Invalid Address! Please try again.'
-            }
-        }
-        if (isValidAddress(this.address) && !isValidUrl(this.nodeUrl)) {
-            return this.inputErrors = {
-                nodeInput: 'Invalid Url! Please try again.'
-            }
-        }
-
-        return false;
-    }
-
     onChange({ target: { name, value } }) {
         this[name] = value;
     }
 
     onClick() {
-        if (!this.getInputErrors()) {
+        if (!isValidUrl(this.nodeUrl)) {
+            this.inputErrors = {
+                nodeInput: 'Invalid Url! Please try again.'
+            }
+            return this.setState({ ...this.inputErrors })
+        } else {
             this.inputErrors = {
                 addressInput: false,
                 nodeInput: false
             }
-            this.address = this.address.replace('0x', '');
+            this.address = this.state.address.replace('0x', '');
             this.balanceRequest = new Xhr(`/address/${this.address}/balance`, {
                 useBaseUrl: this.nodeUrl
             }).result()
@@ -89,49 +80,105 @@ class AccountBalance extends React.Component {
                 .catch(err => console.log('err', err));
 
             this.setState({ ...this.inputErrors })
-        } else {
-            this.setState({ ...this.inputErrors })
         }
     }
 
+    openWallet() {
+        if (!this.password) {
+            this.password = ''
+        }
+        this.setState({
+            disabled: true,
+            loading: true,
+            ...this.inputErrors,
+        });
+
+        this.openWalletWithPassword(this.password);
+
+    }
+
+    openWalletWithPassword(password) {
+        const { encryptedWallet } = getWalletJSON();
+        Wallet.fromEncryptedJSON(encryptedWallet, password)
+            .then(decryptedWallet => {
+                const { address } = decryptedWallet.account;
+                this.setState({
+                    address,
+                    disabled: false,
+                    loading: false,
+                    active: true,
+                    ...this.inputErrors,
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    error: 'Incorrect password!',
+                    disabled: false,
+                    ...this.state,
+                    ...this.inputErrors,
+                });
+            });
+    }
     render() {
-        console.log(this.state)
         return (
             <Layout>
-                <div className="account-balance-container">
-                    <h3 className="component-title">View Account Balance</h3>
-                    <div className="account-balance-inputs">
-                        {
-                            this.inputErrors.addressInput ?
-                                <p>{this.inputErrors.addressInput}</p> : null
-                        }
-                        <TextInput
-                            className="balance-input"
-                            placeholder="Insert your address"
-                            name="address"
-                            onChange={this.onChange}
-                        />
-                        {
-                            this.inputErrors.nodeInput ?
-                                <p>{this.inputErrors.nodeInput}</p> : null
-                        }
-                        <TextInput
-                            className="balance-input margin-top"
-                            placeholder='Insert your node url'
-                            name="nodeUrl"
-                            onChange={this.onChange}
-                        />
-                        <Button
-                            style={styles.button}
-                            onClick={this.onClick}
-                        >DISPLAY BALANCE</Button>
-                        <LogAreaOutput
-                            value={this.state.balanceStatus ? this.balancesRequest : ''}
-                            style={styles.logAreaOutput}
-                            className="margin-top"
-                        />
-                    </div>
-                </div>
+                {
+                    !this.state.active ? (
+                        <div className="send-transaction-container">
+                            <p className="send-transacction-text">PLEASE ENTER YOUR WALLET PASSWORD TO SEE YOUR BALANCE</p>
+                            <div className="open-wallet-input-section">
+                                <TextInput
+                                    name="password"
+                                    className="margin-top"
+                                    type="password"
+                                    onChange={this.onChange}
+                                    placeholder="Password"
+                                />
+                                <Button
+                                    onClick={this.openWallet}
+                                    className="margin-top"
+                                    disabled={this.state.disabled}
+                                    style={styles.button}
+                                >GET BALANCE</Button>
+                            </div>
+                            {this.state.loading ?
+                                <Loader /> : null}
+                        </div>
+                    ) : (
+                            <div className="account-balance-container">
+                                <h3 className="component-title">View Account Balance</h3>
+                                <div className="account-balance-inputs">
+                                    <TextInput
+                                        className="balance-input"
+                                        placeholder="Insert your address"
+                                        name="address"
+                                        value={this.state.address}
+                                        disabled
+                                    />
+                                    {
+                                        this.inputErrors.nodeInput ?
+                                            <p className="error-text">{this.inputErrors.nodeInput}</p> : null
+                                    }
+                                    <TextInput
+                                        className="balance-input margin-top"
+                                        placeholder='Insert your node url'
+                                        name="nodeUrl"
+                                        onChange={this.onChange}
+                                    />
+                                    <Button
+                                        style={styles.button}
+                                        onClick={this.onClick}
+                                        disabled={this.state.disabled}
+                                    >DISPLAY BALANCE</Button>
+                                    <LogAreaOutput
+                                        value={this.state.balanceStatus ? this.balancesRequest : ''}
+                                        style={styles.logAreaOutput}
+                                        className="margin-top"
+                                    />
+                                </div>
+                            </div>
+                        )
+                }
             </Layout>
         );
     }
