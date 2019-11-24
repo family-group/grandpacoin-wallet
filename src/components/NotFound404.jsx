f9bcfb23f435f78cfe06050945995d544821812f
Ariana Ocando <arianaocando@oxoft.io>
2019-11-24 12:21:20 -0400

    added styles to NotFound component

--- src/components/NotFound404.jsx
+++ src/components/NotFound404.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Layout from './Layout';
+import NotFoundIcon from '../assets/logos/error-404.png';
+import './css/NotFound404.css';
 
 const NotFound404 = () => (
     <Layout>
-        404! Url not found!
+        <div className="not-found-container">
+            <div className="not-found-wrapper-icon">
+                <img alt="not-found" src={NotFoundIcon} />
+            </div>
+            <p className="not-found-text">Url Not Found!</p>
+        </div>
     </Layout>
 )
 export default NotFound404;
